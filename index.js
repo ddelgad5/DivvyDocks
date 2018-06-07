18d7cba585c722fd4dcdfd3521d218fc626dac69
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-07 00:36:09 -0500

    Fetchh API data on every page refresh

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const http = require('http');
+const index = fs.readFileSync('index.html');
 const req = require('request-promise-native'); //  Require the request-promise-native package
 const options = {
   url:"https://feeds.divvybikes.com/stations/stations.json",
@@ -11,40 +13,20 @@ const options = {
   },
   json: true
 };
-class Station {
-  constructor(id, stationName, availableDocks, totalDocks, latitude, longitude, statusKey, status, availableBikes, stAddress1, stAddress2, postalCode, testStation, lastCommunicationTime, landMark, is_renting) { // Station object
-    this.id = id,
-    this.stationName = stationName,
-    this.availableDocks = availableDocks,
-    this.totalDocks = totalDocks,
-    this.latitude = latitude,
-    this.longitude = longitude,
-    this.statusKey = statusKey,
-    this.status = status,
-    this.availableBikes = availableBikes,
-    this.stAddress1 = stAddress1,
-    this.stAddress2 = stAddress2,
-    this.postalCode = postalCode,
-    this.testStation = testStation,
-    this.lastCommunicationTime = lastCommunicationTime,
-    this.landMark = landMark,
-    this.is_renting = is_renting;
-  }
-}
 
-app.get('/', function(getRequest, getResponse) { //  Respond to GET request
-  // res.send('Hello World!');  //  Test message
-  console.log("Divvy API data requested");
+app.get('/', function(getRequest, getResponse) { //  Respond to GET request on homepage
+  console.log("Homepage being requested");
+  console.log("Requesting Divvy API data");
   req(options)
     .then( function(rData) {
       console.log("Divvy data received");
-      console.log(rData.executionTime);
-      fs.writeFile('stations.json', JSON.stringify(rData), (err) => {
+      console.log("Timestamp:", rData.executionTime);
+      fs.writeFile('docks.json', JSON.stringify(rData), (err) => {
         if (err) {
           throw err;
         }
       });
-      getResponse.send(rData);
+      getResponse.sendFile(index);
     })
     .catch( function(err) {
       console.log("Something went wrong\n", err);
