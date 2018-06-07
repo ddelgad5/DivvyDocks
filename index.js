1adb7bf62a231b6c07241fd38530aa4f04552263
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-06 19:59:41 -0500

    Add fs and req

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-'use-strict'; // Use strict rules
+'use-strict';  //  Use strict rules
 
 const express = require('express');
 const app = express();
+const fs = require('fs');  //  Require the file system
+const req = require('request-promise-native');  //  Require the request-promise-native package
 
 app.get('/', function (req, res) { //  Respond to GET request
   res.send('Hello World!');
