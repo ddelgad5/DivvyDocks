3a3bb58ee9825bb8d5cfceb230afecdc22a888de
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-06 20:02:44 -0500

    Add Divvy api options and call.

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const express = require('express');
 const app = express();
 const fs = require('fs');  //  Require the file system
 const req = require('request-promise-native');  //  Require the request-promise-native package
+const options = {
+  url:"https://feeds.divvybikes.com/stations/stations.json",
+  headers: {
+    'User-Agent': 'Request-Promise'
+  }
+};
 
-app.get('/', function (req, res) { //  Respond to GET request
-  res.send('Hello World!');
+app.get('/', function(req, res) { //  Respond to GET request
+  // res.send('Hello World!');  //  Test message
+  console.log("Divvy API data requested");
+  req(options) // Divvy API call
+  .then(
+    function(results) {
+      res.send(results);
+      console.log('Divvy request successful!');
+    }
+  )
+  .catch(function(err) {
+    console.log(err);
+  });
 });
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'));
