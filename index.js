fbc96847f6c9826df0ce6b3c557ffe86a0c20ff1
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-06 23:47:55 -0500

    Create class for station object.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const req = require('request-promise-native'); //  Require the request-promise-native package
 const options = {
   url:"https://feeds.divvybikes.com/stations/stations.json",
@@ -10,6 +11,30 @@ const options = {
   },
   json: true
 };
+// class Station {
+//   constructor(id, stationName, availableDocks, totalDocks, latitude, longitude, statusKey, status, availableBikes, stAddress1, stAddress2, postalCode, testStation, lastCommunicationTime, landMark, is_renting) { // Station object
+//     this.id = id,
+//     this.stationName = stationName,
+//     this.availableDocks = availableDocks,
+//     this.totalDocks = totalDocks,
+//     this.latitude = latitude,
+//     this.longitude = longitude,
+//     this.statusKey = statusKey,
+//     this.status = status,
+//     this.availableBikes = availableBikes,
+//     this.stAddress1 = stAddress1,
+//     this.stAddress2 = stAddress2,
+//     this.postalCode = postalCode,
+//     this.testStation = testStation,
+//     this.lastCommunicationTime = lastCommunicationTime,
+//     this.landMark = landMark,
+//     this.is_renting = is_renting;
+//   }
+// }
+// var obj = { //  JSON Structure
+//   executionTime,
+//   stationBeanList: []
+// }
 
 app.get('/', function(getRequest, getResponse) { //  Respond to GET request
   // res.send('Hello World!');  //  Test message
@@ -17,6 +42,7 @@ app.get('/', function(getRequest, getResponse) { //  Respond to GET request
   req(options)
     .then( function(rData) {
       console.log("Divvy data received");
+      console.log(rData[1]);
       getResponse.send(rData);
     })
     .catch( function(err) {
