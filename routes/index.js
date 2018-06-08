ac6d601893dd05386f871a724d7d197e87a88f55
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-07 23:25:51 -0500

    Parsed JSON for required data and send for render.

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,18 @@ var options = {
 router.get('/', function(req, res, next) {
   rpn(options).then( function(results) {
     console.log("Divvy API queried");
-    res.render('index', { syncTime: Date(results.executionTime) });
+    var listChunks = []; // create array to send to template
+    for (let i=0; i in results.stationBeanList; i++) { // iterate through the json file
+      // console.log(results.stationBeanList[i].stationName);
+      var chunk = { // create chunk object to hold individual data
+        stationName: results.stationBeanList[i].stationName,
+        availableDocks: results.stationBeanList[i].availableDocks,
+        availableBikes: results.stationBeanList[i].availableBikes,
+        totalDocks: results.stationBeanList[i].totalDocks
+      }
+      listChunks.push(chunk);
+    }
+    res.render('index', { syncTime: Date(results.executionTime), stations: listChunks });
   });
 });
 
