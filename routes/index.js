3feeac2ed2ccfd8e6c48a15953af49fa3f25ce28
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-07 21:59:08 -0500

    Fetch Divvy API data.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var rpn = require('request-promise-native')
+var options = {
+  url:"https://feeds.divvybikes.com/stations/stations.json",
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  rpn(options).then( function(results) {
+    console.log("Divvy API queried");
+    res.render('index', { syncTime: Date(results.executionTime) });
+  });
 });
 
 module.exports = router;
