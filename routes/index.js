384734df3f9b7fd1d582de717f9c51490568493e
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-16 22:24:43 -0500

    Fix ESLint errors.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const rpn = require('request-promise-native')
+const rpn = require('request-promise-native');
 const options = {
   url:"https://feeds.divvybikes.com/stations/stations.json",
   headers: {
@@ -11,13 +11,13 @@ const options = {
 };
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   rpn(options).then( function(results) {
     console.log("Divvy API queried");
-    var listChunks = []; // create array to send to template
+    let listChunks = []; // create array to send to template
     for (const station of results.stationBeanList) { // iterate through the json file
       // console.log(results.stationBeanList[i].stationName);
-      if (station.postalCode == '60616') {  // Look for stations around IIT
+      if (station.postalCode === '60616') {  // Look for stations around IIT
         let chunk = { // create chunk object to hold individual data
           stationName: station.stationName,
           availableDocks: station.availableDocks,
