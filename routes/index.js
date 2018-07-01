ec9a144eaae600d51bface2172d37ba97ada9ab9
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-30 20:50:58 -0500

    Simplify homepage request

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,11 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const rpn = require('request-promise-native');
-const calc = require('../lib/calc.js');
-const options = {
-  url:"https://feeds.divvybikes.com/stations/stations.json",
-  headers: {
-    'User-Agent': 'Request-Promise'
-  },
-  json: true
-};
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  let coord = calc.getCoord();
-  let query = calc.findByCoord(coord);
-  console.log(query);
-  res.render('index', { syncTime: query.timeStamp, stations: query.listChunks });
-  // res.render('index');
+  console.log("Homepage requested");
+  res.render('index');
 });
 
 module.exports = router;
