fbb53d2d4e54a914533f9f96debfb11b108b7c3c
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-30 19:08:12 -0500

    Render page based off of request.

--- routes/location.js
+++ routes/location.js
@@ -6,6 +6,16 @@ router.post('/', function(req,res) {
   console.log("POST recieved");
   console.log(req.body);
   res.send(req.body);
+  let query;
+  if (req.body.zip) {
+    console.log("User is requesting via zip");
+    query = calc.findByZip(req.body.zip);
+  }
+  else {
+    console.log("User is requesting via coordinates");
+    query = calc.findByCoord(req.body.coord);
+  }
+  res.render('index', { syncTime: query.timeStamp, stations: query.listChunks });
 });
 
 module.exports = router;
