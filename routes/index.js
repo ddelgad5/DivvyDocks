deb328a0e87509d5116740090579d56629739b7d
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-30 19:07:47 -0500

    Simplify initial / request.

--- routes/index.js
+++ routes/index.js
@@ -12,29 +12,11 @@ const options = {
 };
 /* GET home page. */
 router.get('/', function(req, res) {
-  rpn(options).then( function(results) {
-    // console.log("Divvy API queried");
-    let coord = calc.getCoord();
-    let listChunks = []; // create array to send to template
-    for (const station of results.stationBeanList) { // iterate through the json file
-      // console.log(results.stationBeanList[i].stationName);
-      if (station.postalCode === '60616') {  // Look for stations around IIT
-        let chunk = { // create chunk object to hold individual data
-          stationName: station.stationName,
-          availableDocks: station.availableDocks,
-          availableBikes: station.availableBikes,
-          totalDocks: station.totalDocks,
-          lat: station.latitude,
-          long: station.longitude
-        };
-        listChunks.push(chunk);
-      }
-    }
-    calc.chunkSort(listChunks, coord);
-    listChunks = calc.chunkSnip(listChunks, 10);
-    res.render('index', { syncTime: Date(results.executionTime), stations: listChunks });
-  });
-  // res = req.data;
+  let coord = calc.getCoord();
+  let query = calc.findByCoord(coord);
+  console.log(query);
+  res.render('index', { syncTime: query.timeStamp, stations: query.listChunks });
+  // res.render('index');
 });
 
 module.exports = router;
