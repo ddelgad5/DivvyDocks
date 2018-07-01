6a0e581b6accd73f55b04c74630c383e7bcd3317
David Delgado <ddelgad5@hawk.iit.edu>
2018-06-30 20:51:15 -0500

    Add case catching

--- routes/location.js
+++ routes/location.js
@@ -1,21 +1,34 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const calc = require('../lib/calc.js');
 
 router.post('/', function(req,res) {
   console.log("POST recieved");
   console.log(req.body);
-  res.send(req.body);
-  let query;
-  if (req.body.zip) {
+  let locationData = req.body;
+  console.log("Zip:", locationData.zip);
+  console.log("Lat:", locationData.coordLat);
+  console.log("Long:", locationData.coordLong);
+  if (locationData.zip) {
     console.log("User is requesting via zip");
-    query = calc.findByZip(req.body.zip);
+    let query = calc.findByZip(locationData);
+    console.log("Query complete");
+  }
+  else if (locationData.coordLong && locationData.coordLat) {
+    console.log("User is requesting via coordinates");
+    let query = calc.findByCoord(locationData);
+    console.log("Query complete");
   }
   else {
-    console.log("User is requesting via coordinates");
-    query = calc.findByCoord(req.body.coord);
+    console.log("Something went wrong");
+    // Render invalid data
   }
-  res.render('locations', { syncTime: query.timeStamp, stations: query.listChunks });
+  console.log("All location calculations complete");
+  // console.log("Data to be rendered:", query);
+  // res.render('locations', { syncTime: query.timeStamp, stations: query.listChunks });
+  console.log("Data to be rendered:", query);
+  res.render('index');
 });
 
 module.exports = router;
